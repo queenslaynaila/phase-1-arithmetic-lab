d6b40b6b6473e735d24935d02daedce458a8ee39
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-17 01:00:29 +0300

    Done with assignmnet

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+let num3 = 44;
+let num4 = 10;
+let mod = num3 % num4;
+
+console.log(mod);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let max = Math.max(1, 2, 3, 4, 20);
